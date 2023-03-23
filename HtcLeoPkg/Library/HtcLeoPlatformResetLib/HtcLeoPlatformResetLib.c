64db46dc3df054025e8717e5ea8b88a7f7deffa2
Dominik Kobinski <dominikkobinski314@gmail.com>
2023-03-23 11:21:14 +0100

    Library: HtcLeoPlatformResetLib: Fixup

--- HtcLeoPkg/Library/HtcLeoPlatformResetLib/HtcLeoPlatformResetLib.c
+++ HtcLeoPkg/Library/HtcLeoPlatformResetLib/HtcLeoPlatformResetLib.c
@@ -11,17 +11,18 @@
 *  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 *
 **/
-
 #include <Library/DebugLib.h>
 #include <Library/pcom.h>
-#include <Library/qcom_lk.h>
+#include <Library/LKEnvLib.h>
 #include <Uefi.h>
+#include <PiDxe.h>
 
 #include <Library/BaseLib.h>
 #include <Library/MemoryAllocationLib.h>
 #include <Library/IoLib.h>
 #include <Library/PcdLib.h>
 #include <Library/UefiBootServicesTableLib.h>
+#include <Library/EfiResetSystemLib.h>
 #include <Library/BaseMemoryLib.h>
 #include <Library/DebugLib.h>
 
@@ -29,8 +30,7 @@
 #include <Protocol/DevicePath.h>
 #include <Library/pcom.h>
 
-#include <Library/EfiResetSystemLib.h>
-#include <PiDxe.h>
+#include <Library/BootReason.h>
 
 #define LK_BOOTREASON_ADDR 		0x2FFB0000
 #define MARK_LK_TAG 	0X004B4C63
@@ -50,6 +50,81 @@ VOID EFIAPI htcleo_shutdown (){
   msm_proc_comm(PCOM_POWER_DOWN, 0, 0);
 	for (;;) ;
 }
+
+/**
+  This function causes a system-wide reset (cold reset), in which
+  all circuitry within the system returns to its initial state. This type of reset
+  is asynchronous to system operation and operates without regard to
+  cycle boundaries.
+
+  If this function returns, it means that the system does not support cold reset.
+**/
+VOID
+EFIAPI
+ResetCold (
+  VOID
+  )
+{
+  htcleo_reboot(ANDRBOOT_MODE);
+}
+
+/**
+  This function causes a system-wide initialization (warm reset), in which all processors
+  are set to their initial state. Pending cycles are not corrupted.
+
+  If this function returns, it means that the system does not support warm reset.
+**/
+VOID
+EFIAPI
+ResetWarm (
+  VOID
+  )
+{
+    // Map a warm reset into a cold reset
+  DEBUG ((
+    DEBUG_INFO,
+    "Warm reboot not supported by platform, issuing cold reboot\n"
+    ));
+  ResetCold ();
+}
+
+/**
+  This function causes the system to enter a power state equivalent
+  to the ACPI G2/S5 or G3 states.
+
+  If this function returns, it means that the system does not support shutdown reset.
+**/
+VOID
+EFIAPI
+ResetShutdown (
+  VOID
+  )
+{
+  htcleo_shutdown();
+}
+
+/**
+  This function causes a systemwide reset. The exact type of the reset is
+  defined by the EFI_GUID that follows the Null-terminated Unicode string passed
+  into ResetData. If the platform does not recognize the EFI_GUID in ResetData
+  the platform must pick a supported reset type to perform.The platform may
+  optionally log the parameters from any non-normal reset that occurs.
+
+  @param[in]  DataSize   The size, in bytes, of ResetData.
+  @param[in]  ResetData  The data buffer starts with a Null-terminated string,
+                         followed by the EFI_GUID.
+**/
+VOID
+EFIAPI
+ResetPlatformSpecific (
+  IN UINTN  DataSize,
+  IN VOID   *ResetData
+  )
+{
+  // Map the platform specific reset as reboot
+  htcleo_reboot(ANDRBOOT_MODE);
+}
+
 
 /**
   Resets the entire platform.
@@ -76,18 +151,22 @@ LibResetSystem (
 
   switch (ResetType) {
     case EfiResetCold:
-      htcleo_reboot(ANDRBOOT_MODE);
+      ResetCold();
       break;
 
     case EfiResetWarm:
       // not a full power cycle, maybe memory stays around.
       // if not support do the same thing as EfiResetCold.
-      htcleo_reboot(ANDRBOOT_MODE);
+      ResetWarm();
       break;
 
     case EfiResetShutdown:
       // turn off the system.
-      htcleo_shutdown();
+      ResetShutdown();
+      break;
+
+    case EfiResetPlatformSpecific:
+      ResetPlatformSpecific (DataSize, ResetData);
       break;
 
     /* TODO: Add more options for the reboot reason */
