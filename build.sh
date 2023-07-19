3c1a3290877a0e6f51c647bc3eec0b9952985592
J0SH1X <aljoshua.hell@gmail.com>
2023-07-19 15:34:05 +0200

    Build.sh: remopve artefacts from previous build before new one

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # based on the instructions from edk2-platform
+rm -rf ImageResources/*.bin ImageResources/*.img
 set -e
 export PACKAGES_PATH=$PWD/../edk2:$PWD/../edk2-platforms:$PWD
 export WORKSPACE=$PWD/workspace
