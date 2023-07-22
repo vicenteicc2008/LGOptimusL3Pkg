d9addbc84336b7e019d034099ba5425fcfd9bba8
J0SH1X <aljoshua.hell@gmail.com>
2023-07-22 12:49:30 +0200

    HtcLeoPlatformResetLib: add declaration of ResetCold

--- HtcLeoPkg/Include/Library/HtcLeoPlatformResetLib.h
+++ HtcLeoPkg/Include/Library/HtcLeoPlatformResetLib.h
@@ -3,6 +3,7 @@
 
 VOID EFIAPI reboot (unsigned rebootReason);
 VOID EFIAPI htcleo_reboot (unsigned rebootReason);
-VOID EFIAPI htcleo_shutdown ();
+VOID EFIAPI htcleo_shutdown();
+VOID EFIAPI ResetCold (VOID);
 
 #endif
