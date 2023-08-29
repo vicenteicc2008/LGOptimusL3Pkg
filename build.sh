c8f14a75b3b07377cd94ddf6f3d5b6b331ef5d34
Dominduchami <dominikkobinski314@gmail.com>
2023-08-29 12:51:05 +0000

    Project: Support HSPL boot

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # based on the instructions from edk2-platform
-rm -rf ImageResources/*.bin ImageResources/*.img
+rm -rf ImageResources/*.img ImageResources/Tools/*.bin
 set -e
 export PACKAGES_PATH=$PWD/../edk2:$PWD/../edk2-platforms:$PWD
 export WORKSPACE=$PWD/workspace
@@ -11,6 +11,16 @@ GCC5_ARM_PREFIX=arm-linux-gnueabihf- build -s -n 0 -a ARM -t GCC5 -p HtcLeoPkg/H
 chmod +x build_boot_shim.sh
 ./build_boot_shim.sh
 
-cat BootShim/BootShim.bin workspace/Build/QSD8250/DEBUG_GCC5/FV/QSD8250_UEFI.fd >>ImageResources/bootpayload.bin
+cat BootShim/BootShim.bin workspace/Build/QSD8250/DEBUG_GCC5/FV/QSD8250_UEFI.fd >>ImageResources/Tools/lk.bin
 
-mkbootimg --kernel ImageResources/bootpayload.bin --base 0x11800000 --kernel_offset 0x00008000 -o ImageResources/uefi.img
+mkbootimg --kernel ImageResources/Tools/lk.bin --base 0x11800000 --kernel_offset 0x00008000 -o ImageResources/uefi.img
+
+# NBH creation
+if [ ! -f ImageResources/Tools/nbgen ]; then
+	gcc -std=c99 ImageResources/Tools/nbgen.c -o ImageResources/Tools/nbgen
+fi
+
+cd ImageResources/Tools
+./nbgen os.nb
+./yang -F LEOIMG.nbh -f os.nb -t 0x400 -s 64 -d PB8110000 -c 11111111 -v EDK2 -l WWE
+cd ../../
--- build_boot_shim.sh
+++ build_boot_shim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd BootShim
-make UEFI_BASE=0x2C000000 UEFI_SIZE=0x00200000
+make UEFI_BASE=0x2C000000 UEFI_SIZE=0x00100000
 rm BootShim.elf
 cd ..
